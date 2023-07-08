c977b08f791d61c5bb3a1d54c17ba13f10f0a22e
Angelfor3v3r <rainworld@riseup.net>
2023-07-08 06:40:21 -0400

    Cleanup (#23)

--- include/sdkgenny/pointer.hpp
+++ include/sdkgenny/pointer.hpp
@@ -7,8 +7,6 @@ class Pointer : public Reference {
 public:
     explicit Pointer(std::string_view name);
 
-    Pointer* ptr();
-
     void generate_typename_for(std::ostream& os, const Object* obj) const override;
 };
 
--- src/pointer.cpp
+++ src/pointer.cpp
@@ -4,12 +4,6 @@ namespace sdkgenny {
 Pointer::Pointer(std::string_view name) : Reference{name} {
 }
 
-Pointer* Pointer::ptr() {
-    auto p = m_owner->find_or_add<Pointer>(m_name + '*');
-    p->to(this);
-    return p;
-}
-
 void Pointer::generate_typename_for(std::ostream& os, const Object* obj) const {
     m_to->generate_typename_for(os, obj);
     os << "*";
